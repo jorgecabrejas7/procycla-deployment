ef88490f2e250b4ab95450a07b258425c341da50
Apollo <jorgecabrejas7@gmail.com>
2023-06-25 19:20:29 +0200

    Still env...

--- web-procycla/pages/index.jsx
+++ web-procycla/pages/index.jsx
@@ -22,8 +22,7 @@ export default () => {
     useEffect(() => {
         if(session.status != "loading") {
             const loadPlans = async () => {
-                console.log(process.env)
-                console.log(`${process.env.NEXTAUTH_URL} ${process.env.BACK_API_URL} ${process.env.STRIPE_PUBLIC_KEY} ${process.env.BACK_API_AUTH_URL}`)
+                console.log("Keys in enviroment: ", Object.keys(process.env).length);
                 const res = await unprotectedFetch(process.env.BACK_API_URL + "plan", "get", null, t);
                 if(res.status === 200) {
                     const data = await res.json();
